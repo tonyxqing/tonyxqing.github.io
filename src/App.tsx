17d17e35607927361a519f14318bce1435eaa501
tonyxqing <tony.qing@tamu.edu>
2022-03-10 13:30:43 -0600

    new build 3-10-22

--- src/App.tsx
+++ src/App.tsx
@@ -23,13 +23,18 @@ function ButtonAppBar(props: any) {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar sx={{ backgroundColor: "#2c2c2c" }} position="static">
         <Toolbar>
-          <Typography> tonyxqing </Typography>
-          <div style={{ flexGrow: 1 }}></div>
-
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
           {props.children.map((child: any) => {
             return <Button color="inherit">{child}</Button>;
           })}
-
           <div style={{ flexGrow: 1 }}></div>
           <IconButton
             size="large"
